f3cd3aafe80a4990fc19cdc86379a7fa63e48a13
aalexeei <kai2001@ukr.net>
2022-12-05 11:38:05 +0200

    add 5th exercise

--- learning.py
+++ learning.py
@@ -4,14 +4,24 @@ line_lower = f'{"*"*9}\t'
 two_tab = "\t\t*"
 line_vertical = "*\n"*6
 text = f"""
-*{three_tab}*{line}\t{line}\t*{two_tab}\t   **       
-*{three_tab}*{three_tab}*{three_tab}*{two_tab}\t*\t   *    
-*{three_tab}*{three_tab}{three_tab}*{two_tab}\t*\t   *  
+*{three_tab}*{line}\t{line}\t*{two_tab}\t   **
+*{three_tab}*{three_tab}*{three_tab}*{two_tab}\t*\t   *
+*{three_tab}*{three_tab}{three_tab}*{two_tab}\t*\t   *
 *{three_tab}*{line}*{two_tab}{two_tab}{line}\t{line}
-*{three_tab}*{three_tab}   {three_tab}*{two_tab}\t*\t   *  
-*{three_tab}*{three_tab}\t   *\t*{two_tab}\t*\t   *\n{line_lower}{line_lower}{line}\t*{two_tab}\t*\t   *  
+*{three_tab}*{three_tab}   {three_tab}*{two_tab}\t*\t   *
+*{three_tab}*{three_tab}\t   *\t*{two_tab}\t*\t   *\n{line_lower}{line_lower}{line}\t*{two_tab}\t*\t   *
 """
 print(text.replace("*", input("If you want you can enter another symbol as '*' \nIf you don't enter another symbol please enter '*': ")))
 
 
+print("""
+Escape sequences
+\\a      Bell (alert)
+\\b      Backspace
+\\n      New line
+\\t      Horizontal tab
+\\\      Backslash \\
+\\"      Double quotation mark "
+\\'      Single quotation mark '
+""")
 
